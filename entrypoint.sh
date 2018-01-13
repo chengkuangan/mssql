2769db9e0cfd4d1b4f905abdeedf7fee2b0e379e
Gan Cheng Kuan <chgan@redhat.com>
2018-01-13 08:30:45 +0800

    Initial Commit

---
+++ entrypoint.sh
@@ -0,0 +1,2 @@
+#start SQL Server, start the script to create the DB and import the data, start the app
+ACCEPT_EULA=Y MSSQL_PID=Developer SA_PASSWORD=mssql /opt/mssql/bin/sqlservr & /usr/src/app/import-data.sh
