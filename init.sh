738781293169fce1c1838aa2c655cb19a5e3798b
Gan Cheng Kuan <chgan@redhat.com>
2018-01-13 12:47:29 +0800

    Updated init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
 #start SQL Server, start the script to create the DB and import the data, start the app
-ACCEPT_EULA=Y MSSQL_PID=Developer SA_PASSWORD=msSQL1234 /opt/mssql/bin/sqlservr & /usr/src/app/import-data.sh
+/opt/mssql/bin/sqlservr & /usr/src/app/import-data.sh
